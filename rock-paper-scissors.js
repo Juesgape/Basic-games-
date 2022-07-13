95ea79c744e9e77826c964bad63d7b1d90e7a6b7
Juesgape <juesgape11@gmail.com>
2022-07-13 09:06:54 -0500

    Cambios en la interfaz y lógica de Rock Paper Scissors game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,2 +1,24 @@
-let userChoice = document.getElementById("users-choice");
-console.log(userChoice);
+const game = () => {
+    let pScore = 0;
+    let mScore = 0;
+
+    //start the game
+
+    const startGame = () => {
+        const playButton = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playButton.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+    //call all the inner functions
+    startGame();
+    updateScore();
+};
+
+//start the game function
+game();
